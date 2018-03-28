909818c3ca21e1e2f98a2bf0235747de3cde803f
Nicholas Scheurich <nick@scheurich.me>
2018-03-27 22:00:08 -0500

    Add react-ga for Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import ReactGA from "react-ga";
 import App from "./App";
 import groups from "./data/groups.json";
 import registerServiceWorker from "./registerServiceWorker";
 
 import "./styles";
+
+ReactGA.initialize("UA-116383596-1", { debug: true });
+ReactGA.pageview("/");
 
 render(<App groups={groups} />, document.getElementById("root"));
 
