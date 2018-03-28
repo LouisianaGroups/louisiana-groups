791ef342b87921767ed3cf442295e0b449cb1a91
James Alt <james.alt@alterityllc.com>
2018-03-28 18:58:23 -0500

    Swapped to FontAwesome CDN
    - Updated project to use the FontAwesome CDN
    - Deleted fonts from project
    - Removed fontawesome css file

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,5 @@
 import bootsrap from "./bootstrap.css";
 import colofilter from "./colofilter.css";
-import fa from "./fontawesome-all.css";
 import index from "./index.css";
 
-export default { bootsrap, colofilter, fa, index };
+export default { bootsrap, colofilter, index };
