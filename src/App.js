1ee2aa7308849f33ca53c267227ca80412181e77
Nicholas Scheurich <nick@scheurich.me>
2018-03-30 16:23:59 -0500

    Wire up Apollo + GraphQL querying

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { Query } from "react-apollo";
 import GroupCard from "./components/GroupCard";
 import Footer from "./components/Footer";
+import { GROUPS_QUERY } from "./graphql";
 
-const App = ({ groups }) => (
+export default ({ groups }) => (
   <div>
     <div className="row m-0">
-      {groups.map((group, i) => (
-        <GroupCard
-          key={i}
-          Hex={group.Hex}
-          GroupName={group.GroupName}
-          Location={group.Location}
-          Website={group.Website}
-          Facebook={group.Facebook}
-          Twitter={group.Twitter}
-          Meetup={group.Meetup}
-          FontIcon={group.FontIcon}
-        />
-      ))}
+      <Query query={GROUPS_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading…";
+          if (error) return error.message;
+
+          return data.groupsByEventDate.map((group, i) => {
+            if (!group.isActive) return "";
+
+            return (
+              <GroupCard
+                key={i}
+                hex={group.hex}
+                name={group.name}
+                Location={group.location}
+                website={group.website}
+                facebook={group.facebook}
+                twitter={group.twitter}
+                meetup={group.meetup}
+                fontIcon={group.fontIcon}
+                events={group.events}
+                isActive={group.isActive}
+              />
+            );
+          });
+        }}
+      </Query>
     </div>
 
     <Footer />
   </div>
 );
-
-App.propTypes = {
-  groups: PropTypes.array.isRequired
-};
-
-export default App;
