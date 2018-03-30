ff7484fdb385e4b87b7b631d769a69a9ae0ee6a7
Nicholas Scheurich <nick@scheurich.me>
2018-03-30 17:52:28 -0500

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ApolloClient from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
-import moment from "moment-timezone";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
