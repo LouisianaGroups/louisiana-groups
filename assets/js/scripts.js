8b55f6b6875357b9fad7525223dc7b6e5328a6ca
Adam Culpepper <me@adamculpepper.net>
2019-09-01 01:07:45 -0500

    branching from React branch and reverting to before React was added in to get back to the MVP model and setting up SheetrockJS/Handlebars

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,34 +1,56 @@
-/*
-multiline comments go here!
-*/
-
-
-
-var eventData = ko.observableArray();
-
-// data model
-var vm = function() {
-	this.data = $.getJSON();
-};
+// Louisiana Groups
+// www.louisianagroups.com
+// github link
 
 $(function() {
-	ko.applyBindings(new vm()); // This makes Knockout get to work
 
-	$.ajax({
-		type: 'GET',
-		url: 'http://louisianagroups.com/groups.json',
-		success: function(data) {
-			eventData(data);
-			console.table(eventData);
 
-			// fix: KO loop loading before the content loaded
-			setTimeout(function() {
-				$('body').removeClass('loading');
-			}, 0);		},
-		error: function() {
-			alert('error!');
-		}
-	});
+	var sheetGroups = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=1511071343';
+
+	// Load an entire worksheet.
+	var getGroups = function() {
+
+
+
+		var groupsTemplate = Handlebars.compile($('#groups-template').html());
+
+		$('#groups-list').sheetrock({
+			url: sheetGroups,
+			//query: "select A,B,C,AQ,AR",
+			query: "select B,C,D,E,F,G,H,I,J,K",
+			fetchSize: 50,
+			rowTemplate: groupsTemplate,
+			//callback: drawGroups
+		});
+
+	}
+
+	getGroups();
+
+
+
+function drawGroups(error, options, response) {
+  if (error) {
+    console.log('fail');
+  } else{
+    console.log(response.html);
+  }
+  var a = response;
+  console.table(response.rows[0].cellsArray);
+  //debugger;
+}
+
+
+	// Compile the Handlebars template for HR leaders.
+	// var HRTemplate = Handlebars.compile($('#hr-template').html());
+
+	// $('#list').sheetrock({
+	// 	url: mySpreadsheet,
+	// 	//query: "select A,C,D,I order by I desc",
+	// 	query: "select A,B,C,AQ,AR",
+	// 	fetchSize: 50,
+	// 	rowTemplate: HRTemplate
+	// });
 
 });
 
