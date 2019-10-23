85d3134cbf64e66578618614d324a3b739deb230
Adam Culpepper <adam@trendsic.com>
2019-10-23 17:01:02 -0500

    refactoring the drawGroups() function to custom massage a new array out of the returned data for future merge object functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,44 +1,54 @@
 // Louisiana Groups
-// www.louisianagroups.com
-// github link
+// https://louisianagroups.com
+// https://github.com/LouisianaGroups/louisiana-groups
 
 $(function() {
-
-
 	var sheetGroups = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=1511071343';
+	var sheetEvents = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=1511071343';
 
-	// Load an entire worksheet.
 	var getGroups = function() {
-
-
-
 		var groupsTemplate = Handlebars.compile($('#groups-template').html());
 
 		$('#groups-list').sheetrock({
 			url: sheetGroups,
 			//query: "select A,B,C,AQ,AR",
 			query: "select B,C,D,E,F,G,H,I,J,K",
-			fetchSize: 50,
+			fetchSize: 10,
 			rowTemplate: groupsTemplate,
-			//callback: drawGroups
+			callback: drawGroups
 		});
-
 	}
-
 	getGroups();
 
 
+	function drawGroups(error, options, response) {
+		// if (error) {
+		// 	console.log('error!');
+		// } else {
+		// 	console.log(response.html);
+		// }
+		
+		// console.table(response);
+		// console.table(response.rows);
 
-function drawGroups(error, options, response) {
-  if (error) {
-    console.log('fail');
-  } else{
-    console.log(response.html);
-  }
-  var a = response;
-  console.table(response.rows[0].cellsArray);
-  //debugger;
-}
+		var rows = response.rows;
+		var groupsArray = [];		
+
+		for (var i = 1; i < rows.length; i++) {
+			var tempArray = [];
+			var label = rows[i].labels;
+			var value = rows[i].cellsArray;
+
+			for (var x = 0; x < label.length; x++) {
+				tempArray[label[x]] = value[x];
+			}
+
+			groupsArray.push(tempArray);
+		}
+
+		console.warn('groupsArray');
+		console.table(groupsArray);
+	}
 
 
 	// Compile the Handlebars template for HR leaders.
