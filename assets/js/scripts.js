5a0d1454e11a59c7f25b1673191687475683b93a
Adam Culpepper <adam@trendsic.com>
2019-10-31 13:12:52 -0500

    scripts.js
    - removing the DOM elements off the Sheetrock JS calls
    - adding in a feature check for ES6 Promise and displaying a message if browser doesn't support it
    - removing the dataWatcher function in favor of an ES6 promise
    
    index.html
    - adding content loading classes to the <body> tag for visual loading animation
    - adding "$data" to the NextMeetupDateTime data so the app doesn't choke when the data is missing, causing all the data to drop off if the group doesn't have an upcoming event
    - adding the upcoming event link (EventLink) from the data to the Knockout data loop
    - removing the dummy DOM elements for .list-groups and .list-events since the DOM element selector was removed from the JS for Sheetrock's initialization
    - adding back the contributors (Nick and Jimmy) that were removed in the reverting commit
    - correcting the Git link for the project
    
    styles.css
    - adding in the animated loader visual styles
    - removing the dev styles to remove bloat
    - fixing an overflow issue with the footer, causing the page to have a horizontal scrollbar
    - card style fixes and inherit issues

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,10 @@
 var vm = function() {}; // data model
 var groupsData = ko.observableArray();
 
+if (!window.Promise){
+    alert('old browser, upgrade');
+}
+
 $(function() {
 	ko.applyBindings(new vm()); // This makes Knockout get to work
 	var sheetGroups = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=1511071343';
@@ -16,7 +20,7 @@ $(function() {
 	var dataCompleteEvents = false;
 
 	var getGroups = function() {
-		$('#list-groups').sheetrock({
+		sheetrock({
 			url: sheetGroups,
 			query: "select A,B,C,D,E,F,G,H,I,J,K",
 			//fetchSize: 20,
@@ -26,7 +30,7 @@ $(function() {
 	}
 
 	var getEvents = function() {
-		$('#list-events').sheetrock({
+		sheetrock({
 			url: sheetEvents,
 			query: "select B,C,D,E",
 			//fetchSize: 10,
@@ -70,47 +74,6 @@ $(function() {
 		console.table(array);
 	}
 
-	var dataWatcher = setInterval(function() {
-		var dataComplete = false;
-
-		if (dataCompleteGroups == true && dataCompleteEvents == true) {
-			dataComplete = true;
-		}
-
-		if (dataComplete) {
-			clearInterval(dataWatcher);
-
-			//why is the array getting wrapped, requiring this?
-			arrayEvents = arrayEvents.pop();
-			arrayGroups = arrayGroups.pop();
-
-			//sort events array by next event date (for better data massaging)
-			console.warn('sort - before');
-			console.table(arrayEvents);
-			sortArray(arrayEvents, 'NextMeetupDateTime');
-			console.warn('sort - after');
-			console.table(arrayEvents);
-
-			//remove past events from events array
-			arrayEvents = removePastDatesFromArrayByProperty(arrayEvents, 'NextMeetupDateTime');
-			console.warn('remove past events');
-			console.table(arrayEvents);
-			
-			//remove duplicates and only show upcoming event per group
-			arrayEvents = removeDuplicates(arrayEvents, 'GroupID');
-			console.warn('duplicates removed');
-			console.table(arrayEvents);
-
-			//merge the groups and events objects together (deep merge)
-			$.extend(true, arrayMerged, arrayGroups, arrayEvents);
-
-			console.warn('merged array');
-			console.table(arrayMerged);
-
-			groupsData(arrayMerged);
-		}
-	}, 100);
-
 	var removeDuplicates = function(array, prop) {
 		var newArray = [];
 		var lookupObject = {};
@@ -135,7 +98,8 @@ $(function() {
 		return $.grep(array, function (item) {
 			var today = moment();
 			var value = moment(item[prop], 'mm/dd/yyyy HH:mm:ss');
-			var diff = today.diff(value, 'days');
+			var diff = today.diff(item[prop], 'days');
+			//debugger;
 
 			if (diff <= 0) {
 				//console.warn('OLDER | today: ' + today + ' | value: ' + value + ' | diff: ' + today.diff(item[prop], 'days', true));
@@ -146,7 +110,41 @@ $(function() {
 		});
 	}
 
-	getGroups();
-	getEvents();
+	new Promise(function(resolve, reject) {
+		setTimeout(() => resolve(1), 1000);
+		getGroups();
+		getEvents();		
+	}).then(function(result) {
+		//why is the array getting wrapped, requiring this?
+		arrayEvents = arrayEvents.pop();
+		arrayGroups = arrayGroups.pop();
+	}).then(function(result) {
+		//sort events array by next event date (for better data massaging)
+		console.warn('sort - before');
+		console.table(arrayEvents);
+		sortArray(arrayEvents, 'NextMeetupDateTime');
+		console.warn('sort - after');
+		console.table(arrayEvents);
+	}).then(function(result) {
+		//remove past events from events array
+		arrayEvents = removePastDatesFromArrayByProperty(arrayEvents, 'NextMeetupDateTime');
+		console.warn('remove past events');
+		console.table(arrayEvents);
+	}).then(function(result) {
+		//remove duplicates and only show upcoming event per group
+		arrayEvents = removeDuplicates(arrayEvents, 'GroupID');
+		console.warn('duplicates removed');
+		console.table(arrayEvents);
+	}).then(function(result) {
+		//merge the groups and events objects together (deep merge)
+		$.extend(true, arrayMerged, arrayGroups, arrayEvents);
+	}).then(function(result) {
+		console.warn('merged array');
+		console.table(arrayMerged);
+	}).then(function(result) {
+		groupsData(arrayMerged);
+	}).then(function(result) {
+		$('body').removeClass('loading');
+	});
 
 });
