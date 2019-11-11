8801829bd57eafec98b3ddc36887f5f06c57d2f3
Adam Culpepper <adam@trendsic.com>
2019-11-11 17:12:52 -0600

    refactoring the jank promises for Knockout subscriptions for better state management, moving functions around for better scoping, adding in merging per issue #35, populating the Location dropdown dynamically, making the footer stick to the bottom of the page and general clean-up to make the site run snappier

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,30 @@
 
 var vm = function() {}; // data model
 var groupsData = ko.observableArray();
+var stateDataGroupsLoaded = ko.observable(false);
+var stateDataEventsLoaded = ko.observable(false);
+var stateGetEventGroupIDs = ko.observable(false);
+var stateEventsArraySortedByNextMeetup = ko.observable(false);
+var stateGetLastAndNextEventsPerGroup = ko.observable(false);
+var stateRemovePastDatesFromArrayByProperty = ko.observable(false);
+var stateDataLoaded = ko.computed(function() {
+	if (stateDataGroupsLoaded() == true && stateDataEventsLoaded() == true) {
+		return true;
+	}
+});
+var stateGroupsData = ko.computed(function() {
+	if (groupsData().length > 0) {
+		return true;
+	}
+});
+var sheetGroups = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=1511071343';
+var sheetEvents = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=0';
+var today = moment();
+var arrayGroups = [];
+var arrayEvents = [];
+var arrayMerged = [];
+var arrayLocations = [];
+var arrayEventGroupIDs = [];
 
 if (!window.Promise){
     alert('old browser, upgrade');
@@ -11,13 +35,11 @@ if (!window.Promise){
 
 $(function() {
 	ko.applyBindings(new vm()); // init knockout
-	var sheetGroups = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=1511071343';
-	var sheetEvents = 'https://docs.google.com/spreadsheets/d/12zkNdCEEyFeUlIj7Lw_v5-krx3YRws1olJ12N0e8XSU/edit#gid=0';
-	var today = moment();
-	var arrayGroups = [];
-	var arrayEvents = [];
-	var arrayMerged = [];
-	var arrayEventGroupIDs = [];
+
+	var loadData = function() {
+		getGroups();
+		getEvents();
+	}
 
 	var getGroups = function() {
 		sheetrock({
@@ -30,7 +52,7 @@ $(function() {
 	var getEvents = function() {
 		sheetrock({
 			url: sheetEvents,
-			query: "select B,C,D,E",
+			query: "select B,C,D,E order by D desc",
 			callback: callbackEvents
 		});
 	}
@@ -61,77 +83,14 @@ $(function() {
 
 		if (type == 'groups') {
 			arrayGroups.push(array);
+			stateDataGroupsLoaded(true);
 		} else if (type == 'events') {
 			arrayEvents.push(array);
+			stateDataEventsLoaded(true);
 		}
 
 		//console.warn(type + ' array');
 		//console.table(array);
-	}
-
-	var removeDuplicates = function(array, prop) {
-		var newArray = [];
-		var lookupObject = {};
-
-		for (var i in array) {
-			lookupObject[array[i][prop]] = array[i];
-		}
-
-		for (i in lookupObject) {
-			newArray.push(lookupObject[i]);
-		}
-		return newArray;
-	}
-
-	var sortArray = function(array, prop) {
-		array.sort(function(a, b){
-			return new Date(b[prop]) - new Date(a[prop]);
-		});
-	};
-
-	var removePastDatesFromArrayByProperty = function(array, prop) {
-		return $.grep(array, function (item) {
-			var value = moment(item[prop], 'MM/DD/YYYY HH:mm:ss');
-			var diff = today.diff(item[prop], 'days');
-
-			if (diff <= 0) {
-				return item;
-			}
-		});
-	}
-
-	var getEventGroupIDs = function() {
-		for (var i = 0; i < arrayEvents.length; i++) {
-			var groupID = arrayEvents[i].GroupID;
-			var groupName = arrayEvents[i].GroupName;
-			arrayEventGroupIDs.push({GroupID: groupID, GroupName: groupName, LastEventDate: ''});
-		}
-
-		return removeDuplicates(arrayEventGroupIDs, 'GroupID');
-	}
-
-	var getLastEventDatePerGroup = function(array) {
-		$.grep(array, function (item) {
-			var id = item.GroupID;
-			var index = arrayEventGroupIDs.map(function(e) { return e.GroupID; }).indexOf(id);
-			var newDate = item.NextMeetupDateTime;
-			var oldDate = arrayEventGroupIDs[index].LastEventDate || '';
-			var newDateMoment = moment(newDate, 'MM/DD/YYYY HH:mm:ss');
-			var oldDateMoment = moment(oldDate, 'MM/DD/YYYY HH:mm:ss');
-			var newDateDaysAgo = today.diff(newDateMoment, 'days');
-			var oldDateDaysAgo = today.diff(oldDateMoment, 'days');
-
-			if (newDateDaysAgo >= 0) {
-				if (oldDate == '' || newDateDaysAgo < oldDateDaysAgo) {
-					arrayEventGroupIDs[index].LastEventDate = newDate;
-				}
-			}
-		});
-	}
-
-	var countCards = function() {
-		var cardCount = $('#content .card:visible').length;
-		$('#card-count').text(cardCount + ' groups shown');
 	}
 
 	// var getUrlParam = function(name) {
@@ -150,25 +109,146 @@ $(function() {
 	// 	}
 	// }
 
-	var displayCards = function() {
+	var removeDuplicates = function(array, prop) {
+		var newArray = [];
+		var lookupObject = {};
+
+		for (var i in array) {
+			lookupObject[array[i][prop]] = array[i];
+		}
+
+		for (i in lookupObject) {
+			newArray.push(lookupObject[i]);
+		}
+
+		var a = array;
+
+		if (prop == 'GroupID') {
+			//stateGetEventGroupIDs(true);
+			//debugger;
+		// } else if (array == arrayEventGroupIDs) {
+		// 	stateRemovePastDatesFromArrayByProperty(true);
+		} else {
+			//debugger;
+		}
+
+		return newArray;
+	}
+
+	var sortArray = function(array, prop) {
+		array.sort(function(a, b) {
+			return new Date(b[prop]) - new Date(a[prop]);
+		});
+	};
+
+	var mergeArrays = function(array1, array2, prop) {
+		return array1.map(x => ({...x, ...array2.find(y => x[prop] === y[prop])}) );
+	}
+
+	var removePastDatesFromArrayByProperty = function(array, prop) {
+		stateRemovePastDatesFromArrayByProperty(true);
+
+		return $.grep(array, function (item) {
+			var value = moment(item[prop], 'MM/DD/YYYY HH:mm:ss');
+			var diff = today.diff(item[prop], 'days');
+
+			if (diff <= 0) {
+				return item;
+			}
+		});
+	}
+
+	var getEventGroupIDs = function() {
+		for (var i = 0; i < arrayEvents.length; i++) {
+			var groupID = arrayEvents[i].GroupID;
+			var groupName = arrayEvents[i].GroupName;
+			arrayEventGroupIDs.push({GroupID: groupID, GroupName: groupName, LastEventDate: ''}); // remove GroupName later - not needed for the merge
+		}
+
+		stateGetEventGroupIDs(true);
+	}
+
+	var getLastAndNextEventsPerGroup = function(array) {
+		$.grep(array, function (item) {
+			var id = item.GroupID;
+			var index = arrayEventGroupIDs.map(function(e) { return e.GroupID; }).indexOf(id);
+			var date = item.NextMeetupDateTime;
+
+			// REFACTOR THE CODE BELOW INTO A FUNCTION
+
+			// Last Event vars
+			var lastOldDate = arrayEventGroupIDs[index].LastEventDate || '';
+			var lastNewDateMoment = moment(date, 'MM/DD/YYYY HH:mm:ss');
+			var lastOldDateMoment = moment(lastOldDate, 'MM/DD/YYYY HH:mm:ss');
+			var lastNewDateDaysAgo = today.diff(lastNewDateMoment, 'days');
+			var lastOldDateDaysAgo = today.diff(lastOldDateMoment, 'days');
+
+			// Next Event vars
+			var nextOldDate = arrayEventGroupIDs[index].NextEventDate || '';
+			var nextNewDateMoment = moment(date, 'MM/DD/YYYY HH:mm:ss');
+			var nextOldDateMoment = moment(nextOldDate, 'MM/DD/YYYY HH:mm:ss');
+			var nextNewDateDaysAgo = today.diff(nextNewDateMoment, 'days');
+			var nextOldDateDaysAgo = today.diff(nextOldDateMoment, 'days');
+
+			// Last Event condition to add
+			if (lastNewDateDaysAgo >= 0) {
+				if (lastOldDate == '' || lastNewDateDaysAgo < lastOldDateDaysAgo) {
+					arrayEventGroupIDs[index].LastEventDate = date;
+				}
+			}
+
+			// Last Event condition to add
+			if (nextNewDateDaysAgo <= 0) {
+				if (nextOldDate == '' || nextNewDateDaysAgo < nextOldDateDaysAgo) {
+					arrayEventGroupIDs[index].NextEventDate = date;
+				}
+			}
+		});
+
+		stateGetLastAndNextEventsPerGroup(true);
+	}
+
+	var countCards = function() {
+		var cardCount = $('#content .card:visible').length;
+		$('#card-count').text(cardCount + ' groups shown');
+	}
+
+	var uiCleanup = function() {
 		var cards = $('#content .card');
 		var totalCards = $('#content .card').length;
+		var tempArray = [];
+
+		$('#content .card').css({'opacity': 0});
+		$('header #controls').delay(1000).fadeIn();
 
 		for (var i = 0; i < totalCards; i++) {
 			$('#content .card').delay(50).eq(i).animate({'opacity': 1});
 
 			var sel = $('#content .card').eq(i);
 			var background = sel.data('background');
-			var location = sel.data('location');
-			location = location.toLowerCase().replace(/, /g, '|').replace(/ /g , '-').replace(/\|/g, ' ');
-			sel.addClass(location);
+			var locationDirty = sel.data('location');
+			var locationClean = locationDirty.toLowerCase().replace(/, /g, '|').replace(/ /g , '-').replace(/\|/g, ' ');
+
+			sel.addClass(locationClean);
 			sel.css({'background-color': background, 'color': background});
 			sel.find('a').css({'color': background});
+
+			tempArray.push({name: locationDirty, value: locationClean});
 		}
+		
+		arrayLocations = removeDuplicates(tempArray, 'value');
+
+		arrayLocations.forEach(function(item) {
+			var optionHTML = '<option value="' + item.value + '">' + item.name + '</option>';
+			$('#location-selector').append(optionHTML);
+		});
 
 		cards.removeAttr('data-bind');
 		cards.removeAttr('data-background');
 		cards.removeAttr('data-location');
+
+		$('body').removeClass('loading');
+		countCards();
 	}
 
 	var init = function() {
@@ -200,63 +280,68 @@ $(function() {
 		});
 	}
 
-	new Promise(function(resolve, reject) {
-		setTimeout(() => resolve(1), 1000);
-		getGroups();
-		getEvents();		
-	}).then(function(result) {
-		// why is the array getting wrapped, requiring this?
+	stateDataLoaded.subscribe(function(v) {
+		// sort events array by next event date (for better data massaging)
 		arrayEvents = arrayEvents.pop();
 		arrayGroups = arrayGroups.pop();
-	}).then(function(result) {
-		// sort events array by next event date (for better data massaging)
-		//console.warn('sort - before');
-		//console.table(arrayEvents);
-		sortArray(arrayEvents, 'NextMeetupDateTime');
-		//console.warn('sort - after');
-		//console.table(arrayEvents);
-	}).then(function(result) {
-		// get all group IDs in arrayEvents
-		arrayEventGroupIDs = getEventGroupIDs();
-		//console.warn('arrayEventGroupIDs 45435454');
-		//console.table(arrayEventGroupIDs);
-	}).then(function(result) {
-		// insert last group event and insert into arrayEventGroupIDs
-		getLastEventDatePerGroup(arrayEvents);
-		//console.warn('getLastEventDatePerGroup 987054545');
-		//console.table(arrayEventGroupIDs);
-	}).then(function(result) {
-		// remove past events from events array
-		arrayEvents = removePastDatesFromArrayByProperty(arrayEvents, 'NextMeetupDateTime');
-		//console.warn('remove past events');
-		//console.table(arrayEvents);
-	}).then(function(result) {
-		// remove duplicates and only show upcoming event per group
-		arrayEvents = removeDuplicates(arrayEvents, 'GroupID');
-		//console.warn('duplicates removed');
-		//console.table(arrayEvents);
-	// }).then(function(result) {
-	// 	// var testArray = Object.assign({}, arrayEventGroupIDs, arrayGroups);
-	// 	var testArray = $.extend(true, {}, arrayGroups, arrayEventGroupIDs);
-	// 	console.warn('testArray - merge in Last Group Event');
-	// 	console.table(testArray);
 
-	// }).then(function(result) {
-	// 	// merge the groups and events objects together (deep merge)
-	// 	$.extend(true, arrayMerged, arrayGroups, arrayEvents);
-	}).then(function(result) {
-		//dump final array into observable to be used in the Knockout loop in the HTML
-		groupsData(arrayGroups);
-		// console.warn('merged array');
-		// console.table(arrayMerged);
-	}).then(function(result) {
-		$('#content .card').css({'opacity': 0});
-		$('body').removeClass('loading');
-		$('header #controls').delay(1000).fadeIn();
-	}).then(function(result) {
-		countCards();
-		displayCards();
-		init();
+		console.warn('arrayEvents');
+		console.table(arrayEvents);
+
+		// get all group IDs in arrayEvents
+		getEventGroupIDs();
+
+		//arrayEventGroupIDs = removeDuplicates(arrayEventGroupIDs, 'GroupID');
 	});
 
+	stateGetEventGroupIDs.subscribe(function(v) {
+		// get all group IDs in arrayEvents
+		console.warn('arrayEventGroupIDs 691');
+		console.table(arrayEventGroupIDs);
+
+		arrayEventGroupIDs = removeDuplicates(arrayEventGroupIDs, 'GroupID');
+		console.warn('remove duplicates from arrayEventGroupIDs');
+		console.table(arrayEventGroupIDs);
+
+		// insert last group event and insert into arrayEventGroupIDs
+		getLastAndNextEventsPerGroup(arrayEvents);
+	});
+
+	stateGetLastAndNextEventsPerGroup.subscribe(function(v) {
+		console.warn('getLastAndNextEventsPerGroup 914');
+		console.table(arrayEventGroupIDs);
+
+		arrayEvents = removePastDatesFromArrayByProperty(arrayEvents, 'NextMeetupDateTime');
+	});
+
+	stateRemovePastDatesFromArrayByProperty.subscribe(function(v) {
+		arrayMerged = mergeArrays(arrayGroups, arrayEventGroupIDs, 'GroupID');
+		groupsData(arrayMerged);
+	});
+
+	stateGroupsData.subscribe(function(v) {
+		console.warn('merged array');
+		console.table(arrayMerged);
+
+		var countDataGroups = 0;
+		var countDomGroups = 0;
+
+		var waitForElement = function() {
+			countDataGroups = groupsData().length;
+			countDomGroups = $('#content .card').length;
+			console.warn('waitForElement triggered | data:' + countDataGroups + ' | dom:' + countDomGroups);
+
+			if (countDataGroups === countDomGroups) {
+				// SPLIT THESE UP WITH WAITS
+				countCards();
+				uiCleanup();
+				init();
+			} else {
+				setTimeout(waitForElement, 1000);
+			}
+		}
+		waitForElement();
+	});
+
+	loadData();
 });
